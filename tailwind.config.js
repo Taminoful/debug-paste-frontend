519c39cd4442eeecc7a0edd96fb6dca1da58c2c5
Robin C <7423905+Taminoful@users.noreply.github.com>
2021-01-16 02:08:28 +0100

    Initial commit, add project files.

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    purge: {
+        preserveHtmlElements: false,
+        content: [
+            './public/**/*.html',
+            './public/**/*.js'
+        ]
+    },
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                eldoria: {
+                    header: '#5A0009',
+                    background: '#1B2833',
+                    accent: '#11838B',
+                    input: '#B6D1D2'
+                }
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
